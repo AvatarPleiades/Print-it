844cf2f351a17d0780093a709fa2832b40e16daa
AvatarPleiades <berry.julien57@gmail.com>
2023-06-17 23:13:18 +0200

    JS Flèche Suivante

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+//Tableau des images du slider et de leurs descriptions
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -16,3 +17,22 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+//Javascript flèche suivant
+const next = document.querySelector(".arrow_right");
+
+next.addEventListener("click", () => {
+	counter++;
+	if(counter === SlideImg){
+		Dots[SlideImg-1].classList.remove("dot_selected");
+		counter = 0;
+	}
+
+	img.src = "./assets/images/slideshow/" + slides[counter].image;
+	texte.innerHTML = slides[counter].tagLine;
+
+	if(counter != 0 ){
+		Dots[counter-1].classList.remove("dot_selected");
+	}
+	Dots[counter].classList.add("dot_selected");	
+});
